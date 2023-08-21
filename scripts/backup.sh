ced362e4ed72013cf700c4f590793151a0070439
BelalAEzzat <ezzatbilal@Yahoo.com>
2023-08-21 04:37:18 +0400

    debugging

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,17 +3,18 @@
 # Configuration
 env_file=/home/^/umami_prod/docker_cont/.env
 source "$env_file"
+
 # Ensure the backup directory exists
 BACKUP_DIR=/var/backups
 LOG_DIR=/var/backuplogs
 mkdir -p "$BACKUP_DIR"
-mkdir -p "$LOG_FILE"
+mkdir -p "$LOG_DIR"  # Corrected this line to use LOG_DIR
 echo "$POSTGRES_USER" >> "/home/log.txt"
 
 # Generate a timestamp for the backup file
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
-# Define the backup filename
+# Define the backup filename and log filename after defining LOG_DIR
 BACKUP_FILE="$BACKUP_DIR/backup_$TIMESTAMP.sql"
 LOG_FILE="$LOG_DIR/log_$TIMESTAMP.txt"
 
