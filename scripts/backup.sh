ab094630cdbac7e7079c636b8d2af0fc2f25e72a
BelalAEzzat <ezzatbilal@Yahoo.com>
2023-08-21 04:44:43 +0400

    debugging

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-# Configuration
 env_file=/home/^/umami_prod/docker_cont/.env
 source "$env_file"
 
-# Ensure the backup directory exists
 BACKUP_DIR=/var/backups
 LOG_DIR=/var/backuplogs
 mkdir -p "$BACKUP_DIR"
-mkdir -p "$LOG_DIR"  # Corrected this line to use LOG_DIR
+mkdir -p "$LOG_DIR"  
 echo "$POSTGRES_USER" >> "/home/log.txt"
 
-# Generate a timestamp for the backup file
+
 TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
-# Define the backup filename and log filename after defining LOG_DIR
 BACKUP_FILE="$BACKUP_DIR/backup_$TIMESTAMP.sql"
 LOG_FILE="$LOG_DIR/log_$TIMESTAMP.txt"
 
-# Run the backup command
 if docker exec -t "postgres_DB" pg_dump -h localhost -U "$POSTGRES_USER" -d "$POSTGRES_DB" > "$BACKUP_FILE" 2>> "$LOG_FILE"; then
     echo "$(date): Backup successful" >> "$LOG_FILE"
 else
     echo "$(date): Backup failed" >> "$LOG_FILE"
-    # Add additional error handling here, like sending alerts
+   
 fi
